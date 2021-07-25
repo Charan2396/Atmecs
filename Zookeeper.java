07164e3923d3556a3737fefdb29a5b21a71d77c2
Sricharan A <sricharan23@gmail.com>
2021-07-25 04:00:26 -0700

    Update Zookeeper.java

--- Zookeeper.java
+++ Zookeeper.java
@@ -1,10 +1,18 @@
 import java.util.*;
 
 class Node {
+    private String id;
     private String value;
     private ArrayList<Node> children;
     
-    Node(String value) {
+    Node(String id) {
+        this.id = id;
+        this.value = null;
+        this.children = new ArrayList<Node>();
+    }
+    
+    Node(String id, String value) {
+        this.id = id;
         this.value = value;
         this.children = new ArrayList<Node>();
     }
@@ -17,15 +25,19 @@ class Node {
         return this.value;
     }
     
+    public String getId() {
+        return this.id;
+    }
+    
     public ArrayList<Node> getChildren() {
         return this.children;
     }
     
     // If searchValue is found in the children, returns that Node. Else we return null
-    public Node returnChildNode(String searchValue) {
+    public Node returnChildNode(String searchId) {
         for (int i=0;i<this.children.size();i++) {
             Node temp = this.children.get(i);
-            if ( temp.value.equals(searchValue)) {
+            if ( temp.id.equals(searchId)) {
                 return temp;
             }
         }
@@ -33,10 +45,10 @@ class Node {
     }
     
     // If succesful returns newNode, else returns null
-    public Node addChildNode(String value) {
-        Node currentNode = this.returnChildNode(value);
+    public Node addChildNode(String id) {
+        Node currentNode = this.returnChildNode(id);
         if (currentNode==null) {
-            Node newNode = new Node(value);
+            Node newNode = new Node(id);
             this.children.add(newNode);
             return newNode;
         } else {
@@ -64,13 +76,13 @@ class Graph {
         q.add(this.root);
         while(q.size()!=0) {
             currentNode = q.remove(0);
-            System.out.println(currentNode.getValue());
+            System.out.println(currentNode.getId() + " -> " + currentNode.getValue());
             q.addAll(currentNode.getChildren());
         }
         System.out.println("----------------------------");
     }
     
-    public Node create(String path, String value) {
+    public Node create(String path, String newNodeId) {
         String[] pathSplit = path.split("/");
         Node currentNode = this.root;
         if (pathSplit.length != 0) {
@@ -87,7 +99,7 @@ class Graph {
                 return null;
             }
         }
-        Node newNode = currentNode.addChildNode(value);
+        Node newNode = currentNode.addChildNode(newNodeId);
         if (newNode == null) {
             return null;
         }
@@ -96,8 +108,8 @@ class Graph {
     
     public boolean set_value(String path, String value) {
         if(path.equals("/")) {
-            System.out.println("Specified Path refers to root. root value cant be updated");
-            return false;
+            this.root.setValue(value);
+            return true;
         }
         String[] pathSplit = path.split("/");
         Node currentNode = this.root;
@@ -118,15 +130,22 @@ class Graph {
     }
     
     public String get_value(String path) {
+        if(path.equals("/")) {
+            return this.root.getValue();
+        }
         String[] pathSplit = path.split("/");
-        pathSplit[0] = "/";
         Node currentNode = this.root;
-        for(int i=0;i<pathSplit.length;i++) {
-            currentNode = currentNode.returnChildNode(pathSplit[i]);
-            if(currentNode==null) {
-                System.out.println("Specified Path Doesnt Exist");
-                return null;
+        if (pathSplit.length >= 2) {
+            for(int i=1;i<pathSplit.length;i++) {
+                currentNode = currentNode.returnChildNode(pathSplit[i]);
+                if(currentNode==null) {
+                    System.out.println("Specified Path Doesnt Exist");
+                    return null;
+                }
             }
+        } else {
+            System.out.println("Specified Path format is invalid");
+            return null;
         }
         return currentNode.getValue();
     }
@@ -143,15 +162,24 @@ public class Main {
         graph.create("/b", "e");
         graph.create("/a", "c");
         graph.create("/a", "d");
+        graph.create("/a/d", "m");
+        graph.create("/a/d/m", "n");
         graph.printGraph();
         
+        graph.set_value("/", "root");
+        graph.set_value("/a", "aa");
+        graph.set_value("/b", "bb");
+        graph.set_value("/f", "ff");
         graph.set_value("/a/c", "cc");
+        graph.set_value("/a/d", "dd");
+        graph.set_value("/a/d/m", "mm");
+        graph.set_value("/a/d/m", "mmm");
+        graph.set_value("/a/d/m/n", "nn");
         graph.printGraph();
         
-        graph.set_value("/a/c", "cc");
-        graph.printGraph();
+        String val = graph.get_value("/a/d");
+        System.out.println(val);
         
     }
     
 }
-
